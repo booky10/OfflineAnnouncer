1195022ba57b33e6486aaefaeb36bc208d04f38a
booky10 <boooky10@gmail.com>
2022-01-24 13:06:16 +0100

    Shade the dependency and make the jar runnable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
+    application
     `java-library`
     `maven-publish`
+
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "tk.booky"
 version = "1.0.0"
+
+val main = "${group}.${name.toLowerCase()}.${name}Main"
 
 repositories {
     mavenCentral()
@@ -24,4 +29,23 @@ publishing {
         artifactId = project.name.toLowerCase()
         from(components["java"])
     }
+}
+
+tasks {
+    jar {
+        manifest.attributes(
+            "Implementation-Version" to project.version,
+            "Implementation-Title" to project.name,
+            "Implementation-Vendor" to "booky10",
+            "Main-Class" to main,
+        )
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
+application {
+    mainClass.set(main)
 }
---
+++ src/main/java/tk/booky/offlineannouncer/OfflineAnnouncerMain.java
@@ -0,0 +1,9 @@
+package tk.booky.offlineannouncer;
+// Created by booky10 in OfflineAnnouncer (12:59 24.01.22)
+
+public class OfflineAnnouncerMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
